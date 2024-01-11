09be97e703cf59f9f796ee5d60e1feafe85fdda0
oliver7do <afristreaming@gmail.com>
2024-01-11 16:51:20 +0100

    2

--- views/inc/nav.php
+++ views/inc/nav.php
@@ -1,13 +1,11 @@
-<div class="logo">
-    <img src="/views/asset/img/logo.png">
-</div>
-
 <nav class="navbar navbar-expand-lg mb-5" id="boom">
-
+    <div class="logo">
+        <img src="../../../projetfinal/views/asset/img/logo.png">
+    </div>
 
     <div class="container-fluid">
 
-        <a class="navbar-brand" href="http://localhost/argentproject/accueil.php" id="navlet">Apprenect</a>
+        <a class="navbar-brand" href="http://localhost/" id="navlet">Apprenect</a>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
 
@@ -15,34 +13,26 @@
 
         </button>
 
-        <div class="collapse navbar-collapse justify-content-end" id="navbarText">
+        <div class="collapse navbar-collapse justify-content-end" class="nav0" id="navbarText">
 
             <ul class="navbar-nav mb-2 mb-lg-0">
-                <?php if (isset($_SESSION['id_user'])) { ?>
-                    <li class="nav-item">
-                        <?php if ($_SESSION['role'] == "admin") { ?>
-                            <a class="nav-link active" aria-current="page" href="http://localhost/argentproject/pageadmin/list_clients.php" id="navlet">CLIENTS</a>
-                        <?php } ?>
-                    </li>
-                <?php } ?>
                 <li class="nav-item">
-
-                    <a class="nav-link active" aria-current="page" href="http://localhost/argentproject/proposdenous.php" id="navlet">A PROPOS DE NOUS</a>
-
+                    <a class="nav-link active" aria-current="page" href="http://localhost/argentproject/proposdenous.php" id="navlet">Important</a>
                 </li>
-
                 <li class="nav-item">
-
-                    <a class="nav-link active" aria-current="page" href="http://localhost/argentproject/inscription.php" id="navlet">INSCRIPTION</a>
-
+                    <a class="nav-link active" aria-current="page" href="http://localhost/argentproject/inscription.php" id="navlet">Prix</a>
                 </li>
-
                 <li class="nav-item">
-
-                    <a class="nav-link active" aria-current="page" href="http://localhost/argentproject/connexion.php" id="navlet">CONNEXION</a>
-
+                    <a class="nav-link active" aria-current="page" href="http://localhost/argentproject/inscription.php" id="navlet">A propos de nous</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="http://localhost/argentproject/connexion.php" id="navlet">Connexion</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="http://localhost/argentproject/connexion.php" id="navlet">s'inscrire</a>
                 </li>
             </ul>
+
         </div>
     </div>
 </nav>
